c16356e2dbd61fba13b3172e890f0c8a35bbee5d
girish <girishgodage@gmail.com>
2021-02-02 10:07:49 +0530

    Initial commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    "login": "bdayioglu",
+    "id": 49947032,
+    "node_id": "MDQ6VXNlcjQ5OTQ3MDMy",
+    "avatar_url": "https://avatars.githubusercontent.com/u/49947032?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/bdayioglu",
+    "html_url": "https://github.com/bdayioglu",
+    "followers_url": "https://api.github.com/users/bdayioglu/followers",
+    "following_url": "https://api.github.com/users/bdayioglu/following{/other_user}",
+    "gists_url": "https://api.github.com/users/bdayioglu/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/bdayioglu/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/bdayioglu/subscriptions",
+    "organizations_url": "https://api.github.com/users/bdayioglu/orgs",
+    "repos_url": "https://api.github.com/users/bdayioglu/repos",
+    "events_url": "https://api.github.com/users/bdayioglu/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/bdayioglu/received_events",
+    "type": "User",
+    "site_admin": false
+    },
+];
